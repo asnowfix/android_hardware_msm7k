5db89cf8888ffcf1aaa098380418c07b517a7d10
Francois-Xavier KOWALSKI <fix.kowalski@gmail.com>
2010-04-04 16:43:40 +0200

    fix bt (no)include mismatch

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE:= libaudiopolicy
 
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+ifeq ($(strip $(BOARD_HAVE_BLUETOOTH)),true)
   LOCAL_CFLAGS += -DWITH_A2DP
 endif
 
@@ -45,7 +45,7 @@ LOCAL_SRC_FILES += AudioHardware.cpp
 LOCAL_CFLAGS += -fno-short-enums
 
 LOCAL_STATIC_LIBRARIES += libaudiointerface
-ifeq ($(BOARD_HAVE_BLUETOOTH),true)
+ifeq ($(strip $(BOARD_HAVE_BLUETOOTH)),true)
   LOCAL_SHARED_LIBRARIES += liba2dp
 endif
 
